5f42d62bcce65b7bef6d9ce9491681fdb9fd2b1f
muchamadkurniawan <muchamad.kurniawan@itats.ac.id>
2025-03-17 12:53:58 +0700

    complite the test and main program

--- view_testing.go
+++ view_testing.go
@@ -1,23 +1,23 @@
-package main
+// package main
 
-import (
-	"array_struct1/view"
-	"fmt"
-)
+// import (
+// 	"array_struct1/view"
+// 	"fmt"
+// )
 
-func main() {
-	view.Insert()
-	view.Insert()
-	view.Views()
-	fmt.Println()
+// func main() {
+// 	view.Insert()
+// 	view.Insert()
+// 	view.Views()
+// 	fmt.Println()
 
-	//// test update
-	// view.Update()
-	// view.Views()
-	// fmt.Println()
+// 	//// test update
+// 	// view.Update()
+// 	// view.Views()
+// 	// fmt.Println()
 
-	// test delete
-	view.Delete()
-	view.Views()
+// 	// test delete
+// 	view.Delete()
+// 	view.Views()
 
-}
+// }
